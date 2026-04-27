480de7b50325150318ac23a51d6f982e6b7ccfb9
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-04-27 10:46:12 -0600

    feta: Agregar sección de formulario

--- src/shared/components/layout/Navbar.jsx
+++ src/shared/components/layout/Navbar.jsx
@@ -1,9 +1,11 @@
 import imgLogo from "../../../assets/img/kinal_sports.png";
+import { AvatarUser } from "../../ui/AvatarUser";
 
 export const Navbar = () => {
   return (
     <nav className="bg-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
+        
         {/* Logo + título */}
         <div className="flex items-center gap-2">
           <img
@@ -18,7 +20,7 @@ export const Navbar = () => {
         </div>
 
         {/* Avatar placeholder */}
-        <div className="w-10 h-10 rounded-full bg-gray-200" />
+        <AvatarUser />
       </div>
     </nav>
   );
